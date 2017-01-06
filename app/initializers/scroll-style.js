9cf8ca644f099a4e2c2d2ea9ef9fcf6ead432058
Scott Batson <sjbats@email.com>
2017-01-06 17:42:27 -0500

    add initializer to set box-shadow on nav

---
+++ app/initializers/scroll-style.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+const { $ } = Ember;
+
+export function initialize() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) {
+      $('.nav-bar').addClass('box-shadow');
+    } else {
+      $('.nav-bar').removeClass('box-shadow');
+    }
+  });
+}
+
+export default {
+  name: 'scroll-style',
+  initialize
+};
