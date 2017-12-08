66f85a534d4dac8765955208c6053005b21f501f
Scott Batson <sjbats@email.com>
2017-12-08 17:36:37 -0500

    hide chart with too few wagers

--- app/controllers/round.js
+++ app/controllers/round.js
@@ -3,10 +3,12 @@ import Ember from 'ember';
 const {
   Controller,
   computed,
+  computed: { lte },
   get
 } = Ember;
 
 export default Ember.Controller.extend({
+  notEnoughWagers: lte('sortedWagers.length', 1),
   sortedWagers: computed('wagers.@each.amount', function() {
     return get(this, 'wagers').sortBy('amount');
   })
