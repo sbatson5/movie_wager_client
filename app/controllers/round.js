0405bf817d530fde8af5c0395d52a612a9a2b6fd
Scott Batson <sjbats@email.com>
2018-04-20 13:38:09 -0400

    order by place - hide vert chart

--- app/controllers/round.js
+++ app/controllers/round.js
@@ -6,5 +6,14 @@ export default Controller.extend({
   notEnoughWagers: lte('sortedWagers.length', 1),
   sortedWagers: computed('wagers.@each.amount', function() {
     return get(this, 'wagers').sortBy('amount');
+  }),
+
+  wagersByPlaceOrAmount: computed('wagers.@each.{amount,place}', function() {
+    let wagers = get(this, 'wagers');
+    let firstWagerPlace = get(wagers, 'firstObject.place');
+    if (firstWagerPlace) {
+      return wagers.sortBy('place');
+    }
+    return wagers.sortBy('amount');
   })
 });
