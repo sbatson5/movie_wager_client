d79e0969962fd6a331508567b6d30b2813d816fc
Scott Batson <sjbats@email.com>
2018-05-18 13:29:45 -0400

    update admin form (#30)

--- app/models/round.js
+++ app/models/round.js
@@ -3,7 +3,6 @@ import attr from 'ember-data/attr';
 
 export default Model.extend({
   code: attr('string'),
-  startDate: attr('date'),
   endDate: attr('date'),
   boxOfficeAmount: attr('number'),
   title: attr('string')
