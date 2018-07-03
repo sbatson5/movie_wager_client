4246f770eb38c508c5ffc9037f9991daa80229a9
Scott Batson <sjbats@email.com>
2018-07-03 13:33:22 -0400

    update header-bar component (#33)

--- app/components/header-bar.js
+++ app/components/header-bar.js
@@ -2,10 +2,21 @@ import Component from '@ember/component';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
 
+const DAYS = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+
 export default Component.extend({
   tagName: 'header',
   classNames: ['header'],
   navToggle: service('nav-toggle'),
+
+  init() {
+    this._super(...arguments);
+    let today = new Date();
+    this.dayOfTheWeek = DAYS[today.getDay()];
+    this.month = MONTHS[today.getMonth()];
+    this.day = today.getDate();
+  },
 
   actions: {
     logoutUser() {
